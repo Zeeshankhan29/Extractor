944afcab0616b26516ea3719912e52ce4b32ba1c
ZeeshanKhan29 <zeeshankhan29khan@gmail.com>
2023-09-11 15:10:09 +0530

    Code optimized to extract pdf

--- src/extractor/components/data_ingestion.py
+++ src/extractor/components/data_ingestion.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pdfplumber
 import os
 import sys
+import fitz
 import cv2
 import shutil
 import PyPDF2
@@ -25,6 +26,7 @@ class DataIngestion:
         self.curr_dir = Path(os.getcwd())
         self.data_dir = self.dataingestion.data_dir
         self.ner_model = spacy.load("en_core_web_sm")
+        self.file_dir = self.dataingestion.file_dir
         
         
     def normal_image(im_file_path):
@@ -68,44 +70,6 @@ class DataIngestion:
         
         return image
 
-    def jpg_file1(self, img_path):
-        img = cv2.imdecode(np.frombuffer(img_path.read(), np.uint8), 1)
-        ocr = PaddleOCR(use_angle_cls=True, lang='en')
-        result = ocr.ocr(img, cls=True)
-
-        # Extract text from the result
-        extracted_text = " ".join([line[1][0] for line in result[0]])
-        print(extracted_text)
-        doc = self.ner_model(extracted_text)
-        for data in doc.ents:
-            print(data.text,data.start,data.end,data.label_)
-
-        displacy.serve(doc,style='ent')
-
-        exit(0)
-        # Extract item names and prices using spaCy NER
-        doc = self.ner_model(extracted_text)
-        items_and_prices = []
-        
-        item_name = ""
-        price = ""
-        items_and_prices =[]
-        for token in doc:
-            if token.ent_type_ == "PRODUCT":
-                item_name = token.text
-            elif token.ent_type_ == "MONEY":
-                price = token.text
-                if item_name:  # Check if there is a valid item name
-                    items_and_prices.append({"item": item_name, "price": price})
-                    item_name = ""  
-        # Create DataFrame and save files
-        file_name = Path(img_path.name)
-        file1 = file_name.stem
-        df = pd.DataFrame(items_and_prices)
-        csv_file_name = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.csv'))
-        df.to_csv(csv_file_name, index=False)
-        excel_file_name = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.xlsx'))
-        df.to_excel(excel_file_name, index=False)
 
     def jpg_file(self,img_path):
         img = cv2.imdecode(np.frombuffer(img_path.read(), np.uint8), 1)
@@ -115,6 +79,7 @@ class DataIngestion:
         result = ocr.ocr(img, cls=True)
         file_name = Path(img_path.name)
         file1 = file_name.stem
+        print(file1)
         result = result[0]
         boxes = [line[0] for line in result]
         txts = [line[1][0] for line in result]
@@ -132,63 +97,89 @@ class DataIngestion:
         csv_file_name = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.csv'))
         df.to_csv(csv_file_name, index=False)
 
+        df1 = pd.read_csv(csv_file_name)
+        values=[]
+        for data in df1.values:
+            if data[0].isdigit()==False:
+                data[0]= data[0].replace('/-', '').replace('Rs', '').replace('/', '')
+                values.append(data[0])
+            else:
+                values.append(data[0])
+
+        first_valid=[]
+        for item in range(0,(len(values)-1)):
+        #     print((values[item],values[item+1]))
+            first_valid.append((values[item],values[item+1]))
+        
+        final_valid = []
+        for index_value in range(len(first_valid) - 1):
+            first_item = first_valid[index_value][0]
+            second_item = first_valid[index_value][1]
+            if (isinstance(first_item, str) and not first_item.isdigit()) and (isinstance(second_item, str) and all(char.isdigit() or char.isspace() for char in second_item)):
+                final_valid.append((first_item, second_item))
+        df2 = pd.DataFrame(final_valid, columns=['Item Name', 'Price'])   
+        valid_path = Path(os.path.join(self.curr_dir,self.file_dir,f'{file1}.csv')) 
+        df2.to_csv(valid_path,index=False)  
         # Saving DataFrame to Excel
-        excel_file_name = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.xlsx'))
-        df.to_excel(excel_file_name, index=False)
+        excel_file_name = Path(os.path.join(self.curr_dir, self.file_dir, f'{file1}.xlsx'))
+        df2.to_excel(excel_file_name, index=False)
 
 
     def pdf_file(self, uploaded_file):
-        extracted_text = ""
+        try:
+            extracted_text = ""
 
-        with uploaded_file as pdf_file:
-            pdf_reader = PyPDF2.PdfReader(pdf_file)
-            for page in pdf_reader.pages:
-                extracted_text += page.extract_text()
+            with uploaded_file as pdf_file:
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
+                for page in pdf_reader.pages:
+                    extracted_text += page.extract_text()
 
-        file_name = Path(uploaded_file.name)
-        file1 = file_name.stem
+            file_name = Path(uploaded_file.name)
+            file1 = file_name.stem
 
-    # Rest of your processing code (creating Excel workbook, populating sheet, saving Excel file, etc.)
-
-
-
-        data_list=[]
-        for i in extracted_text.split('\n'):
-            if i.endswith('/-'):
-                data_list.append(i) 
+        # Rest of your processing code (creating Excel workbook, populating sheet, saving Excel file, etc.)
 
 
 
+            data_list=[]
+            for i in extracted_text.split('\n'):
+                if i.endswith('/-'):
+                    i=i.replace('/-', '').replace('Rs', '').replace('/', '').replace('.','')
+                    data_list.append(i) 
+
+            # Apply the function to the 'Text' column and create new columns
+            if data_list == []:
+                st.write('Pdf is not editable please try with jpg file format instead')
+                exit(0)
+            else:       
+                df = pd.DataFrame({'Text':data_list})
+                # Saving DataFrame to CSV
+                csv_file_name = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.csv'))
+                df.to_csv(csv_file_name, index=False)
+                
+                print(data_list)
+                menu_items = []
+
+                for item in data_list:
+                    match = re.search(r'^(.*?)\s+(\d+)$', item)
+                    if match:
+                        menu_name = match.group(1).strip()
+                        price = match.group(2).strip()
+                        menu_items.append({'menu_name': menu_name, 'price': price})
+
+                # Now, menu_items is a list of dictionaries, where each dictionary contains 'menu_name' and 'price' keys.
+                # You can access the data like this:
+
+                df1= pd.DataFrame(menu_items)
+                print(df1)
+                # Saving DataFrame to CSV
+                csv_file_name = Path(os.path.join(self.curr_dir, self.file_dir, f'{file1}.xlsx'))
+                df1.to_excel(csv_file_name, index=False)
+        
+        except Exception as e:
+            print(e)
 
 
-        # Create a new Excel workbook
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
-        sheet.title = file1
-        # Write the header row
-        sheet["A1"] = "Item Name"
-        sheet["B1"] = "Price(s)"
-
-        # Populate the Excel sheet with data
-        row_num = 2  # Start from the second row
-        for item in data_list:
-            # Extract item name and prices using regex
-            match = re.match(r'^(.+?)\s+((?:\d+/-\s*)+)', item)
-            if match:
-                item_name = match.group(1)
-                prices = match.group(2)
-
-                sheet.cell(row=row_num, column=1, value=item_name)
-                sheet.cell(row=row_num, column=2, value=prices)
-
-                row_num += 1
-
-        # Save the Excel file
-        excel_path = Path(os.path.join(self.curr_dir, self.data_dir, f'{file1}.xlsx'))
-        workbook.save(excel_path)
-
-
-        print(f"Data saved to '{excel_path}'")
 
 
     def text_extraction(self, img_path):
@@ -196,10 +187,11 @@ class DataIngestion:
 
         if img_extension.lower() == '.pdf':
             self.pdf_file(img_path)
-        elif img_extension.lower() in ['.jpg', '.jpeg', '.png']:
+        if img_extension.lower() in ['.jpg', '.jpeg', '.png']:
             self.jpg_file(img_path)
         else:
             print("Unsupported image format")
+
 
 
     def run_streamlit(self):
@@ -208,29 +200,31 @@ class DataIngestion:
         uploaded_file = st.file_uploader("Choose an image or PDF file", type=["jpg", "jpeg", "png", "pdf"])
 
         if uploaded_file is not None:
-            st.write("File uploaded successfully!")
-
-            file_extension = uploaded_file.name.split('.')[-1].lower()
-            self.text_extraction(uploaded_file)
-
-            st.write("Download Processed Files:")
-            if file_extension == "pdf":
-                download_link = self.generate_download_button(uploaded_file, "xlsx")
-            elif file_extension in ["jpg", "jpeg", "png"]:
-                # download_link_csv = self.generate_download_button(uploaded_file, "csv")
-                download_link_xlsx = self.generate_download_button(uploaded_file, "xlsx")
-                # download_link_txt = self.generate_download_button(uploaded_file, "txt")
-                # st.markdown(download_link_csv, unsafe_allow_html=True)
-                st.markdown(download_link_xlsx, unsafe_allow_html=True)
-                # st.markdown(download_link_txt, unsafe_allow_html=True)
-            else:
-                st.write("Unsupported file format for download")
-                st.markdown(download_link, unsafe_allow_html=True)
+            try:
+                st.write("File uploaded successfully!")
+                st.write("Wait to Download the excel file!")
+                file_extension = uploaded_file.name.split('.')[-1].lower()
+                self.text_extraction(uploaded_file)
+                st.write("Click below to download the excel file")
+                if file_extension == "pdf":
+                    download_link = self.generate_download_button(uploaded_file, "xlsx")
+                elif file_extension in ["jpg", "jpeg", "png"]:
+                    # download_link_csv = self.generate_download_button(uploaded_file, "csv")
+                    download_link_xlsx = self.generate_download_button(uploaded_file, "xlsx")
+                    # download_link_txt = self.generate_download_button(uploaded_file, "txt")
+                    # st.markdown(download_link_csv, unsafe_allow_html=True)
+                    st.markdown(download_link_xlsx, unsafe_allow_html=True)
+                    # st.markdown(download_link_txt, unsafe_allow_html=True)
+                else:
+                    st.write("Unsupported file format for download")
+                    st.markdown(download_link, unsafe_allow_html=True)
+            except:
+                st.write('Something went wrong with file download try again with different file formats ')
 
 
     def generate_download_button(self, uploaded_file, file_extension):
         file_name = uploaded_file.name.split('.')[0]
-        processed_file_path = Path(self.curr_dir) / self.data_dir / f"{file_name}.{file_extension}"
+        processed_file_path = Path(self.curr_dir) / self.file_dir / f"{file_name}.{file_extension}"
         return st.download_button(
             label=f"Download {file_name}.{file_extension}",
             data=processed_file_path.read_bytes(),
--- src/extractor/config/configuration.py
+++ src/extractor/config/configuration.py
@@ -10,15 +10,17 @@ class Configuration:
         logging.info(f'loading config yaml configuration file: {config_file_path}')
         self.config = read_yaml(config_file_path)
         create_directories([self.config.artifacts_root])
-
+     
 
     def get_data_ingestion_config(self):
         config = self.config.data_ingestion
         create_directories([config.data_dir])
+        create_directories([config.file_dir])
         
 
 
         data_ingestion  = DataIngestionConfig(
-                             data_dir=config.data_dir)
+                             data_dir=config.data_dir,
+                             file_dir=config.file_dir)
         return data_ingestion
     
